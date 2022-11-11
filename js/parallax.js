17b3a8aef27db8f54871061dcce66d0b384113cb
guiiwfz <97guilherme.alves@gmail.com>
2022-11-11 20:30:36 -0300

    a

---
+++ js/parallax.js
@@ -0,0 +1,33 @@
+$(window).scroll(function() {
+  parallax();
+})
+
+function parallax() {
+
+  var wScroll = $(window).scrollTop()
+
+
+  $('.parallax--bg').css('background-position', 'center ' + (wScroll*0.75)+'px');
+
+
+  $('.parallax--box').css('top', -5 + (wScroll*0.9)+'em')
+
+}
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
